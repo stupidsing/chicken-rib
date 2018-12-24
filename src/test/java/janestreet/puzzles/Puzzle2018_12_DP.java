732fc5da2663d5c3bd47cc181210b093dbc09875
ywsing <stupidsing@gmail.com>
2018-12-25 06:57:51 +0800

    -

--- src/test/java/janestreet/puzzles/Puzzle2018_12_DP.java
+++ src/test/java/janestreet/puzzles/Puzzle2018_12_DP.java
@@ -17,16 +17,6 @@ import suite.util.To;
 /*
 for all tile order, find the first score
 
-<<<<<<< HEAD
-  3, 24,  6,  4,  2, 15,  5,
- 15,  5,  4,  2,  8,  3,  6,
-  4, 12,  7, 14,  5,  2,  3,
-  2,  3, 15,  5, 30,  6,  4,
-  8,  4,  2,  3,  6,  5, 20,
-  5,  6,  3,  1,  4, 24,  2,
- 10,  2,  5, 15,  3,  4,  8,
-SCORE = 230
-=======
  35,  6,  3,  5,  4,  8,  2,
   5,  7, 18,  2, 20,  4,  3,
   3,  4,  5, 10,  2, 18,  6,
@@ -35,7 +25,6 @@ SCORE = 230
   4,  2, 14,  1,  3, 15,  5,
  20,  5,  2,  4,  8,  3, 15,
 SCORE = 229
->>>>>>> 842b4b0d8c0cdf67f9f166a53df8c3476a7c3f92
  */
 // https://www.janestreet.com/puzzles/block-party-2/
 public class Puzzle2018_12_DP {
