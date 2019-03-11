facbfce2e782a2bfea99d6e721854c7163e5598e
stupidsing@gmail.com <stupidsing@gmail.com>
2019-03-11 10:24:44 +0800

    -

--- src/test/java/chickenrib/btree/PbTreeTest.java
+++ src/test/java/chickenrib/btree/PbTreeTest.java
@@ -3,7 +3,6 @@ package chickenrib.btree;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,17 +11,13 @@ import org.junit.Test;
 
 import chickenrib.btree.impl.PbTreeBuilder;
 import chickenrib.btree.impl.PbTreeConfiguration;
-import chickenrib.btree.impl.PbTreeImpl;
 import chickenrib.btree.impl.PbTreeStack;
 import suite.cfg.Defaults;
-import suite.fs.KeyDataMutator;
 import suite.fs.KeyDataStore;
-import suite.fs.KeyValueMutator;
 import suite.node.util.Singleton;
 import suite.object.Object_;
 import suite.serialize.Serialize;
 import suite.serialize.Serialize.Serializer;
-import suite.streamlet.FunUtil.Source;
 import suite.streamlet.Outlet;
 import suite.util.List_;
 import suite.util.To;
@@ -34,14 +29,14 @@ public class PbTreeTest {
 
 	@Test
 	public void testSimple() throws IOException {
-		PbTreeConfiguration<Integer> config = newIbTreeConfiguration("pbTree-stack", serialize.int_);
+		var config = newIbTreeConfiguration("pbTree-stack", serialize.int_);
 		config.setCapacity(65536);
 
-		try (PbTreeStack<Integer> pbTreeStack = new PbTreeStack<>(config)) {
-			PbTree<Integer> pbTree = pbTreeStack.getIbTree();
+		try (var pbTreeStack = new PbTreeStack<>(config)) {
+			var pbTree = pbTreeStack.getIbTree();
 			pbTree.create().end(true);
-			KeyDataStore<Integer> store = pbTree.begin();
-			KeyValueMutator<Integer, Integer> mutator = store.mutate();
+			var store = pbTree.begin();
+			var mutator = store.mutate();
 
 			for (int i = 0; i < 32; i++)
 				mutator.put(i, i);
@@ -54,22 +49,22 @@ public class PbTreeTest {
 
 	@Test
 	public void testSingleLevel() throws IOException {
-		PbTreeConfiguration<Integer> config = newIbTreeConfiguration("pbTree-single", serialize.int_);
-		PbTreeBuilder builder = new PbTreeBuilder(config);
+		var config = newIbTreeConfiguration("pbTree-single", serialize.int_);
+		var builder = new PbTreeBuilder(config);
 
-		try (PbTree<Integer> pbTree = builder.buildTree(Defaults.tmp.resolve("pbTree-single"), config, null)) {
+		try (var pbTree = builder.buildTree(Defaults.tmp.resolve("pbTree-single"), config, null)) {
 			pbTree.create().end(true);
 
-			KeyDataStore<Integer> store = pbTree.begin();
-			KeyValueMutator<Integer, Integer> kvm = store.mutate();
-			KeyDataMutator<Integer> kdm = store.mutateData();
+			var store = pbTree.begin();
+			var kvm = store.mutate();
+			var kdm = store.mutateData();
 			int size = pbTree.guaranteedCapacity();
 
-			for (int i = 0; i < size; i++)
+			for (var i = 0; i < size; i++)
 				kdm.putTerminal(i);
-			for (int i = size - 1; 0 <= i; i--)
+			for (var i = size - 1; 0 <= i; i--)
 				kvm.remove(i);
-			for (int i = 0; i < size; i++)
+			for (var i = 0; i < size; i++)
 				kdm.putTerminal(i);
 
 			assertEquals(size, dumpAndCount(store));
@@ -78,16 +73,16 @@ public class PbTreeTest {
 
 	@Test
 	public void testMultipleLevels() throws IOException {
-		PbTreeConfiguration<String> config = newIbTreeConfiguration("pbTree-multi", serialize.string(16));
-		PbTreeBuilder builder = new PbTreeBuilder(config);
+		var config = newIbTreeConfiguration("pbTree-multi", serialize.string(16));
+		var builder = new PbTreeBuilder(config);
 
 		int i = 0;
-		Path p0 = Defaults.tmp.resolve("pbTreeMulti" + i++);
-		Path p1 = Defaults.tmp.resolve("pbTreeMulti" + i++);
-		Path p2 = Defaults.tmp.resolve("pbTreeMulti" + i++);
+		var p0 = Defaults.tmp.resolve("pbTreeMulti" + i++);
+		var p1 = Defaults.tmp.resolve("pbTreeMulti" + i++);
+		var p2 = Defaults.tmp.resolve("pbTreeMulti" + i++);
 
-		try (PbTreeImpl<Integer> pbTree0 = builder.buildAllocationIbTree(p0);
-				PbTreeImpl<Integer> pbTree1 = builder.buildAllocationIbTree(p1, pbTree0);
+		try (var pbTree0 = builder.buildAllocationIbTree(p0);
+				var pbTree1 = builder.buildAllocationIbTree(p1, pbTree0);
 				PbTree<String> pbTree2 = builder.buildTree(p2, config, pbTree1)) {
 			test(pbTree2);
 		}
@@ -95,17 +90,17 @@ public class PbTreeTest {
 
 	@Test
 	public void testStack() throws IOException {
-		PbTreeConfiguration<String> config = newIbTreeConfiguration("pbTree-stack", serialize.string(16));
+		var config = newIbTreeConfiguration("pbTree-stack", serialize.string(16));
 		config.setCapacity(65536);
 
-		try (PbTreeStack<String> pbTreeStack = new PbTreeStack<>(config)) {
+		try (var pbTreeStack = new PbTreeStack<String>(config)) {
 			test(pbTreeStack.getIbTree());
 		}
 	}
 
 	private <Key extends Comparable<? super Key>> PbTreeConfiguration<Key> newIbTreeConfiguration( //
 			String name, Serializer<Key> serializer) {
-		PbTreeConfiguration<Key> config = new PbTreeConfiguration<>();
+		var config = new PbTreeConfiguration<Key>();
 		config.setComparator(Object_::compare);
 		config.setPathPrefix(Defaults.tmp.resolve(name));
 		config.setPageSize(pageSize);
@@ -130,10 +125,10 @@ public class PbTreeTest {
 		// we would run out of allocatable pages. Here we limit ourself to
 		// updating 25 keys each.
 
-		for (Outlet<String> subset : Outlet.of(list).chunk(25)) {
-			KeyDataStore<String> store = pbTree.begin();
-			KeyDataMutator<String> mutator = store.mutateData();
-			for (String s : subset)
+		for (var subset : Outlet.of(list).chunk(25)) {
+			var store = pbTree.begin();
+			var mutator = store.mutateData();
+			for (var s : subset)
 				mutator.putTerminal(s);
 			store.end(true);
 		}
@@ -142,10 +137,10 @@ public class PbTreeTest {
 
 		Collections.shuffle(list);
 
-		for (List<String> subset : List_.chunk(list, 25)) {
-			KeyDataStore<String> store = pbTree.begin();
-			KeyValueMutator<String, Integer> mutator = store.mutate();
-			for (String s : subset)
+		for (var subset : List_.chunk(list, 25)) {
+			var store = pbTree.begin();
+			var mutator = store.mutate();
+			for (var s : subset)
 				mutator.remove(s);
 			store.end(true);
 		}
@@ -154,9 +149,9 @@ public class PbTreeTest {
 	}
 
 	private int dumpAndCount(KeyDataStore<?> store) {
-		Source<?> source = store.mutate().keys(null, null).source();
+		var source = store.mutate().keys(null, null).source();
+		var count = 0;
 		Object object;
-		int count = 0;
 
 		while ((object = source.g()) != null) {
 			System.out.println(object.toString());
